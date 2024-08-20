26d33a332f1b1bc7101dc97f4d8210b262897a81
aya-cs22 <aya786930@gmail.com>
2024-08-20 09:06:50 +0300

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,12 +6,7 @@ import csv
 import math
 from typing import List, Tuple
 
-def index_range(page: int, page_size: int) -> tuple:
-    """return a tuple of size two containing
-    a start index and an end index"""
-    start_index = page_size * (page - 1)
-    end_index = page_size + start_index
-    return (start_index, end_index)
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -31,25 +26,25 @@ class Server:
 
         return self.__dataset
 
-    # @staticmethod
-    # def index_range(page: int, page_size: int) -> tuple:
-    #     """return a tuple of size two containing
-    #     a start index and an end index"""
-    #     start_index = page_size * (page - 1)
-    #     end_index = page_size + start_index
-    #     return (start_index, end_index)
+    @staticmethod
+    def index_range(page: int, page_size: int) -> tuple:
+        """return a tuple of size two containing
+        a start index and an end index"""
+        start_index = page_size * (page - 1)
+        end_index = page_size + start_index
+        return (start_index, end_index)
 
-    # def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    #     assert isinstance(page, int) and page > 0
-    #     assert isinstance(page_size, int) and page_size > 0
-    #     dataset = self.dataset()
-    #     total_size_page = math.ceil(len(dataset) / page_size)
-    #     if page > total_size_page:
-    #         return []
-    #     start_index, end_index = self.index_range(page, page_size)
-    #     if start_index >= len(dataset):
-    #         return []
-    #     return dataset[start_index:end_index]
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        dataset = self.dataset()
+        total_size_page = math.ceil(len(dataset) / page_size)
+        if page > total_size_page:
+            return []
+        start_index, end_index = self.index_range(page, page_size)
+        if start_index >= len(dataset):
+            return []
+        return dataset[start_index:end_index]
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''
         Retrieve a specific page of data.
@@ -79,9 +74,3 @@ class Server:
             'prev_page' : page - 1 if page > 1 else None,
             'total_page' : total_page
         }
-# page_size
-# page
-# data
-# next_page
-# prev_page
-# total_pages
