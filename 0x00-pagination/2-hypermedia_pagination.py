b54bd7325d3502b154ae604150335efdec3b84ac
aya-cs22 <aya786930@gmail.com>
2024-08-20 09:16:50 +0300

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -59,36 +59,19 @@ class Server:
             return []
         return data_set[n_index_range[0]: n_index_range[1]]
     
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        '''returns a dict of metadata'''
-        data = self.get_page(page=page, page_size=page_size)
-        data_len = len(data)
-        data_set = self.dataset()
-        total_set_size = math.ceil(len(data_set) / page_size)
-        if page == 1:
-            prev_page = None
-        else:
-            prev_page = page - 1
-        if page < total_set_size:
-            next_page = page + 1
-        else:
-            next_page = None
-        return {'page_size': page_size, 'page': page, 'data': data,
-                'next_page': next_page, 'prev_page': prev_page,
-                'total_pages': total_set_size}
 
-    # def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-    #     dataset = self.dataset()
-    #     total_item = len(dataset)
-    #     total_page = math.ceil(len(dataset) / page_size)
-    #     start_index = (page - 1) * page_size
-    #     end_index = start_index + page_size
-    #     page_data = self.get_page(page, page_size)
-    #     return {
-    #         'page_size' : page_size,
-    #         'page' : page,
-    #         'data' : page_data,
-    #         'next_page' : page + 1 if page < total_page else None,
-    #         'prev_page' : page - 1 if page > 1 else None,
-    #         'total_page' : total_page
-    #     }
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        dataset = self.dataset()
+        # total_item = len(dataset)
+        total_page = math.ceil(len(dataset) / page_size)
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        page_data = self.get_page(page, page_size)
+        return {
+            'page_size' : page_size,
+            'page' : page,
+            'data' : page_data,
+            'next_page' : page + 1 if page < total_page else None,
+            'prev_page' : page - 1 if page > 1 else None,
+            'total_page' : total_page
+        }
