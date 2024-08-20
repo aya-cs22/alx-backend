24565413743b47abdc99024606c9d5361494ded6
aya-cs22 <aya786930@gmail.com>
2024-08-20 08:31:51 +0300

    Simple pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+
+
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    @staticmethod
+    def index_range(page: int, page_size: int) -> tuple:
+        """return a tuple of size two containing
+        a start index and an end index"""
+        start_index = page_size * (page - 1)
+        end_index = page_size + start_index
+        return (start_index, end_index)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        dataset = self.dataset()
+        total_size_page = math.ceil(len(dataset) / page_size)
+        if page > total_size_page:
+            return []
+        start_index, end_index = self.index_range(page, page_size)
+        if start_index >= len(dataset):
+            return []
+        return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
