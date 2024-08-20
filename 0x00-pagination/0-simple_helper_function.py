9ccea41e90c4d6dfde445d294430c2fc88b19993
aya-cs22 <aya786930@gmail.com>
2024-08-20 06:55:28 +0300

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,6 @@
 
 def index_range(page, page_size):
     """ return a tuple of size two containing a start index and an end index"""
-    start_index = page_size * (1 - page)
-    end_index = page_size - start_index
+    start_index = page_size * (page - 1)
+    end_index = page_size + start_index
     return (start_index, end_index)
