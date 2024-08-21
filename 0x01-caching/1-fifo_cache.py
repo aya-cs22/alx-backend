ae5693df0f13d7518f68920c2d9954c2c1d9af21
aya-cs22 <aya786930@gmail.com>
2024-08-21 10:50:31 +0300

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,6 +8,7 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """Initialize the class"""
         super().__init__()
+        # To keep track of the order of keys in the dictionary self.cache_data
         self.order = []
 
     def put(self, key, item):
@@ -16,34 +17,7 @@ class FIFOCache(BaseCaching):
             if key not in self.cache_data:
                 self.order.append(key)
             self.cache_data[key] = item
-            
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 oldest_key = self.order.pop(0)
                 del self.cache_data[oldest_key]
                 print(f"DISCARD: {oldest_key}")
-
-    def get(self, key):
-        """Get an item from the cache"""
-        return self.cache_data.get(key) if key is not None else None
-# #!/usr/bin/python3
-# """FIFO caching"""
-# BaseCaching = __import__('base_caching').BaseCaching
-
-
-# class FIFOCache(BaseCaching):
-#     """ inherits from BaseCaching and is a caching system"""
-#     def __init__(self):
-#         super().__init__()
-#         # To keep track of the order of keys in the dictionary self.cache_data
-#         self.order = []
-
-#     def put(self, key, item):
-#         """Add an item in the cache"""
-#         if key is not None and item is not None:
-#             if key not in self.cache_data:
-#                 self.order.append(key)
-#             self.cache_data[key] = item
-#             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-#                 oldest_key = self.order.pop(0)
-#                 del self.cache_data[oldest_key]
-#                 print(f"DISCARD: {oldest_key}")
