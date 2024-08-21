9c52b8f271fba1a5b45678492ee7a38b50c4e2bc
aya-cs22 <aya786930@gmail.com>
2024-08-21 10:43:43 +0300

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""FIFO caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        super().__init__()
+        # To keep track of the order of keys in the dictionary self.cache_data
+        self.order = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if key not in self.cache_data:
+                self.order.append(key)
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            oldest_key = self.order.pop(0)
+            del self.cache_data[oldest_key]
+            print(f"DISCARD: {oldest_key}")
