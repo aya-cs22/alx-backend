62221063468e8847c4d50c0988d59b702c8cdd23
aya-cs22 <aya786930@gmail.com>
2024-08-21 11:24:55 +0300

    FIFO caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,8 +4,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """LIFOCache inherits from BaseCaching and implements a LIFO caching system"""
-    
+    """LIFOCache inherits from BaseCaching ,implements a LIFO caching system"""
     def __init__(self):
         """Initialize the class"""
         super().__init__()
@@ -15,44 +14,16 @@ class LIFOCache(BaseCaching):
         """Add an item in the cache"""
         if key is not None and item is not None:
             if key in self.cache_data:
-                self.order.remove(key)  # Remove the key if it's already in cache
+                # Remove the key if it's already in cache
+                self.order.remove(key)
             self.order.append(key)
             self.cache_data[key] = item
 
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last_key = self.order.pop(-2)  # Remove the second last item in the order
+                last_key = self.order.pop(-2)
                 del self.cache_data[last_key]
                 print(f"DISCARD: {last_key}")
 
     def get(self, key):
         """Get an item from the cache"""
         return self.cache_data.get(key) if key is not None else None
-# #!/usr/bin/python3
-# """LIFO caching"""
-# BaseCaching = __import__('base_caching').BaseCaching
-
-
-# class LIFOCache(BaseCaching):
-#     """ inherits from BaseCaching and is a caching system"""
-#     def __init__(self):
-#         """Initialize the class"""
-#         super().__init__()
-#         # To keep track of the order of keys in the dictionary self.cache_data
-#         self.order = []
-
-#     def put(self, key, item):
-#         """Add an item in the cache"""
-#         if key is not None and item is not None:
-#             if key not in self.cache_data:
-#                 self.order.append(key)
-#             self.cache_data[key] = item
-#             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-#                 oldest_key = self.order.pop()
-#                 del self.cache_data[oldest_key]
-#                 print(f"DISCARD: {oldest_key}")
-
-#     def get(self, key):
-#         """Get an item from the cache"""
-#         if key is not None:
-#             return self.cache_data.get(key)
-#         return None
