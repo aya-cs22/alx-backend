1510024eaad767bbffa53c5c015b49345c2c373d
aya-cs22 <aya786930@gmail.com>
2024-08-21 11:55:35 +0300

    LRU Caching caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """LRU Caching"""
-BaseCaching = __import__('base_caching').BaseCaching
 from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """LRUCache inherits from BaseCaching and implements an LRU caching system"""
+    """LRUCache inherits from BaseCaching, implements an LRU caching system"""
 
     def __init__(self):
         """Initialize the class"""
@@ -16,19 +16,15 @@ class LRUCache(BaseCaching):
         """Add an item in the cache"""
         if key is not None and item is not None:
             if key in self.cache_data:
-                # If the key is already in cache, move it to the end to mark it as recently used
                 self.cache_data.move_to_end(key)
             self.cache_data[key] = item
-
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                # Remove the least recently used item (the first item in the OrderedDict)
-                lru_key, _ = self.cache_data.pop(0)
-                print(f"DISCARD: {lru_key}")
+                oldest_key = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {oldest_key}")
 
     def get(self, key):
         """Get an item from the cache"""
         if key is not None and key in self.cache_data:
-            # Move the accessed item to the end to mark it as recently used
             self.cache_data.move_to_end(key)
             return self.cache_data[key]
         return None
