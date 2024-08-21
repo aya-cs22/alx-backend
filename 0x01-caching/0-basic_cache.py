7f9a58c0c22c5bcd637203fd5776f2e419e3cf29
aya-cs22 <aya786930@gmail.com>
2024-08-21 09:58:36 +0300

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,15 +5,14 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """inherits from BaseCaching and is a caching system"""
-    
+
     def put(self, key, item):
         """Add an item in the cache"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """Get an item from the cache"""
         if key is not None:
             return self.cache_data.get(key)
-        else:
-            return None
-
+        return None
