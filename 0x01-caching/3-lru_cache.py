d53062c15f7fe73404b003c5d055d8894301a0e9
aya-cs22 <aya786930@gmail.com>
2024-08-21 11:32:34 +0300

    LRU Caching caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" LRU Caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        """Initialize the class"""
+        super().__init__()
+        # To keep track of the order of keys in the dictionary self.cache_data
+        self.order = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if key not in self.cache_data:
+                self.order.append(key)
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                oldest_key = self.order.pop(0)
+                del self.cache_data[oldest_key]
+                print(f"DISCARD: {oldest_key}")
+
+    def get(self, key):
+        """Get an item from the cache"""
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
