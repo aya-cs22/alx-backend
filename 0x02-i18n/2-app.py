2892efd239c1d0353705cd0d27b99ea14d9fc874
aya-cs22 <aya786930@gmail.com>
2024-08-28 04:07:32 +0300

    app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config:
@@ -12,8 +13,6 @@ class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 
 
 app.config.from_object(Config)
