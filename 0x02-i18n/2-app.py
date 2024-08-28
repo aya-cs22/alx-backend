0a11c6fd29da64729b38b60cc153cafa622801c0
aya-cs22 <aya786930@gmail.com>
2024-08-28 04:04:44 +0300

    app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,33 +1,79 @@
 #!/usr/bin/env python3
-'''Basic Babel setup'''
+""" A script for basic flask integration"""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config:
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
+    """
+    Configuration class for the application.
+        Attributes:
+        Languages (list): List of supported languages.
+    """
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
 
 
 app.config.from_object(Config)
-babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """get local"""
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/")
-def index():
-    """html"""
-    return render_template('2-index.html')
+def hello():
+    """This function returns the rendered template for the index.html page.
+
+    Returns:
+        The rendered template for the index.html page.
+    """
+    return render_template('1-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
+# #!/usr/bin/env python3
+# '''Basic Babel setup'''
+
+# from flask import Flask, render_template, request
+# from flask_babel import Babel
+
+# app = Flask(__name__)
+
+
+# class Config:
+#     LANGUAGES = ['en', 'fr']
+#     BABEL_DEFAULT_LOCALE = 'en'
+#     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# app.config.from_object(Config)
+# babel = Babel(app)
+
+
+# @babel.localeselector
+# def get_locale():
+#     """get local"""
+#     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+# @app.route("/")
+# def index():
+#     """html"""
+#     return render_template('2-index.html')
+
+
+# if __name__ == "__main__":
+#     app.run()
