8c7a9cc4737ccbaa378b3bc2755acc40dda4791c
aya-cs22 <aya786930@gmail.com>
2024-08-28 03:31:43 +0300

    app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,20 +3,24 @@
 
 from flask import Flask, render_template
 from flask_babel import Babel
-
 app = Flask(__name__)
+
 
 class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
 babel = Babel(app)
+
+
 @app.route('/')
 def index():
     """html"""
     return render_template('1-index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
