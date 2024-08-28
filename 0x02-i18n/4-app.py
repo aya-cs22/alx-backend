455a2e3383a056b27e9074952babe3605cddd043
aya-cs22 <aya786930@gmail.com>
2024-08-28 04:14:32 +0300

    app

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,80 +1,33 @@
-#!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import (
-    Flask,
-    render_template,
-    request
-)
-from flask_babel import Babel
-
-
-class Config(object):
-    """
-    Configuration for Babel
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
+from flask import Flask, render_template, request, g
+from flask_babelex import Babel
 
 app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app.config.from_object(Config)
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """
-    Select and return best language match based on supported languages
-    """
-    loc = request.args.get('locale')
-    if loc in app.config['LANGUAGES']:
-        return loc
+    """get local"""
+    local = request.args.get('locale')
+    if local in app.config['LANGUAGES']:
+        return local
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
+@app.route('/')
+def index():
+    """html"""
     return render_template('4-index.html')
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
-# from flask import Flask, render_template, request, g
-# from flask_babelex import Babel
-
-# app = Flask(__name__)
-
-
-# class Config:
-#     LANGUAGES = ['en', 'fr']
-#     BABEL_DEFAULT_LOCALE = 'en'
-#     BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-# app.config.from_object(Config)
-# babel = Babel(app)
-
-
-# @babel.localeselector
-# def get_locale():
-#     """get local"""
-#     local = request.args.get('locale')
-#     if local in app.config['LANGUAGES']:
-#         return local
-#     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-# @app.route('/')
-# def index():
-#     """html"""
-#     return render_template('4-index.html')
-
-
-# if __name__ == "__main__":
-#     app.run()
+    app.run()
