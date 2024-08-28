b6d381664be06e850d93bd672e4425f15621017c
aya-cs22 <aya786930@gmail.com>
2024-08-28 03:56:04 +0300

    app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -14,20 +14,3 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
-
-# #!/usr/bin/env python3
-# '''Task 0: Basic Flask app'''
-
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def hello():
-#     """html"""
-#     return render_template('0-index.html')
-
-
-# if __name__ == "__main__":
-#     app.run()
