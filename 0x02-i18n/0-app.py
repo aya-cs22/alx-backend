64bff3edc9ef0a130f633bf8d40b8856ce75f08c
aya-cs22 <aya786930@gmail.com>
2024-08-28 03:48:01 +0300

    app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-'''Task 0: Basic Flask app'''
+""" A script for basic flask integration"""
 
 from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     """html"""
     return render_template('0-index.html')
@@ -14,3 +15,20 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
+
+# #!/usr/bin/env python3
+# '''Task 0: Basic Flask app'''
+
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello():
+#     """html"""
+#     return render_template('0-index.html')
+
+
+# if __name__ == "__main__":
+#     app.run()
