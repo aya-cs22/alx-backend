5a16af4be8879207cba5cae0445544344f1d76e5
aya-cs22 <aya786930@gmail.com>
2024-08-28 03:39:19 +0300

    app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
-'''Task 0: Basic Flask app
-'''
+""" A script for basic flask integration"""
 
 from flask import Flask, render_template
+from flask_babel import Babel
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """html"""
+@app.route("/")
+def hello():
+    """This function returns the rendered template for the index.html page.
+
+    Returns:
+        The rendered template for the index.html page.
+    """
     return render_template('0-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run()
+# #!/usr/bin/env python3
+# '''Task 0: Basic Flask app
+# '''
+
+# from flask import Flask, render_template
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#     """html"""
+#     return render_template('0-index.html')
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5000)
